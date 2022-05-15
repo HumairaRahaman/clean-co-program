108097fabbf31bc714bc1967d745f0e3e7e810a0
Humaira Rahaman <humairamitul@gmail.com>
2022-05-16 03:15:42 +0600

    landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from "react"
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Services from "./pages/Services"
+import { publicRoute } from "./routes/publicRoutes"
+
 
 function App() {
+  useEffect(()=>{
+    AOS.init();
+  },[])
   return (
     
       <Navbar>
        <Routes>
-         <Route path="/" element={<Home></Home>}></Route>
+         {/* <Route path="/" element={<Home></Home>}></Route>
          <Route path="/about" element={<About></About>}></Route>
          <Route path="/services" element={<Services></Services>}></Route>
          <Route path="/contact" element={<Contact></Contact>}></Route>
-         <Route path="/login" element={<Login></Login>}></Route>
+         <Route path="/login" element={<Login></Login>}></Route> */}
+         {
+           publicRoute.map(({path,Component},index)=>(<Route key={index} path={path} element={<Component/>}/>))
+         }
        </Routes>
       </Navbar>
     
