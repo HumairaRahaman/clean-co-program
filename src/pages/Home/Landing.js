3c2f2a47be7fe4288dbb618d5b821ffd183c5d50
Humaira Rahaman <humairamitul@gmail.com>
2022-05-18 19:59:42 +0600

    route create consef

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -20,7 +20,7 @@ const Landing = () => {
         </div>
       </div>
     </div>
-    <div className=" shadow-lg p-10 w-5/6  bg-neutral mt-[-30px] z-20 relative rounded-2xl mx-auto">
+    <div className=" shadow-lg p-10 w-5/6 bg-base-100 mt-[-30px] z-20 relative rounded-2xl mx-auto">
         <h1 className=" text-2xl mb-5 text-primary">Get Free Estimate</h1>
         <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
         <input type="text" placeholder="Type here" className="input input-bordered w-[260px]" />
